8b9527a6f792dd8c487f0cdced6746bce01ad0dc
Abubakar A Abubakar <auduabubakar1992@gmail.com>
2019-02-04 22:53:24 +0100

    Second Commit

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+
+mongoose.connect("mongodb://localhost/belpcamp", {useNewUrlParser: true}).then(()=>{
+    console.log("Database Connected Successfully");
+}).catch(err => console.log(err));
+
+const   app = express(),
+        port = 3000,
+        hostname = "localhost";
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(indexRouter);
+
+app
