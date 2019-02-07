c05396e1fdb7eb8f60b21cacfb71afc1ef55fc7d
Abubakar A Abubakar <auduabubakar1992@gmail.com>
2019-02-07 12:19:03 +0100

    Cookies

--- app.js
+++ app.js
@@ -26,28 +26,44 @@ app.use(cookieParser("12345-67890-09876-54321"));
 function auth(req,res,next){
     console.log(req.signedCookies);
 
-    var authHeader = req.header.authorization;
+    if(!req.signedCookies.user){
+        let authHeader = req.headers.authorization;
 
-    if(!authHeader){
-        let err = new Error('You are not authenticated');
-        res.setHeader('WWW-Authenticate', 'Basic');
-        err.status = 401;
-        return next(err);
+        if(!authHeader){
+            let err = new Error('You are not authenticated');
+            res.setHeader('WWW-Authenticate', 'Basic');
+            err.status = 401;
+            return next(err);
+        }
+
+        let auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+
+        let username = auth[0];
+        let password = auth[1];
+
+        if(username === "admin" && password === "password"){
+            res.cookie('user', 'admin', { signed: true})
+            next();
+        }else{
+            let err = new Error('You are not authenticated');
+            res.setHeader('WWW-Authenticate', 'Basic');
+            err.status = 401;
+            return next(err);
+        }
+    }
+    else{
+        if(req.signedCookies.user === 'admin'){
+            next();
+        }
+        else{
+            let err = new Error('You are not authenticated');
+            
+            err.status = 401;
+            return next(err);
+        }
     }
 
-    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-
-    var username = auth[0];
-    var password = auth[1];
-
-    if(username === "admin" && password === "password"){
-        next();
-    }else{
-        let err = new Error('You are not authenticated');
-        res.setHeader('WWW-Authenticate', 'Basic');
-        err.status = 401;
-        return next(err);
-    }
+    
 }
 
 app.use(auth);
